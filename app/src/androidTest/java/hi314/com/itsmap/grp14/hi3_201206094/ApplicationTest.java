543deeeb39a13f0c65d6cf6e90c99ecf62851aae
Moonbloom <201271236@iha.dk>
2015-04-17 15:08:58 +0200

    first commit simple setup

---
+++ app/src/androidTest/java/hi314/com/itsmap/grp14/hi3_201206094/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hi314.com.itsmap.grp14.hi3_201206094;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/hi314/com/itsmap/grp14/hi3_201206094/MainActivity.java
@@ -0,0 +1,13 @@
+package hi314.com.itsmap.grp14.hi3_201206094;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
